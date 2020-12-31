21c944740e43b047354658477f837206f212402c
chenbin <553474628@qq.com>
2020-12-31 17:07:17 +0800

    接入单元测试

---
+++ test/Test.java
@@ -0,0 +1,25 @@
+import java.io.UnsupportedEncodingException;
+
+public class Test {
+
+    @org.junit.Test
+    public void testSha256() {
+        String str = "123456789";
+        try {
+            String encodeStr = SHA256Utils.sha256Hex(str.getBytes("UTF-8"));
+            System.out.println(encodeStr);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @org.junit.Test
+    public void testBox() {
+        Box box = new Box();
+        box.setId(1);
+        box.setTimestamp(System.currentTimeMillis());
+        box.setPreBoxHash("0");
+        String result = box.matchIdAndTimeStampAndPreBoxHash();
+        System.out.println(result);
+    }
+}
